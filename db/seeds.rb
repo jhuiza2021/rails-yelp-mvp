990374ab4dedadffd6e094c96f3cab7ce658ffa9
Jesus Huiza <jesus_huiza@hotmail.com>
2021-02-17 21:01:27 -0400

    dia 02-17 ejercicio 2 restaurant y review

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Restaurant.destroy_all
+puts "comienza a crear " 
+rest1 = Restaurant.create!(name: "sushi", address: "las mercedes", category: "japanese" )
+rest2 = Restaurant.create!(name: "fu shu", address: "altamira", category: "chinese" )
+rest3 = Restaurant.create!(name: "le petit", address: "LPG", category: "french" )
+rest4 = Restaurant.create!(name: "pizza", address: "candelaria", category: "belgian" )
+rest5 = Restaurant.create!(name: "mama mia", address: "castellana", category: "italian" )
+puts "termina de  crear " 
+
